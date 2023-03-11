f3d44bb875a2b21dc3fd2aa3a0a9355aedd413fd
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2023-03-11 21:21:14 +0300

    add Location model

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    address = models.CharField('Адрес места', max_length=200, unique=True)
+    lat = models.DecimalField('Широта', max_digits=9, decimal_places=6)
+    lon = models.DecimalField('Долгота', max_digits=9, decimal_places=6)
+    query_date = models.DateTimeField('Дата запроса')
+
+    def __str__(self):
+        return f'{self.address} {self.lat} {self.lon}'

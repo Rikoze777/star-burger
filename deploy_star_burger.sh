80c74c643bfb63025312cb686d89e45877d58193
Rikoze777 <spawn00007@gmail.com>
2023-05-06 19:00:25 +0500

    fix script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -6,7 +6,7 @@ git pull
 . ./venv/bin/activate
 pip install -r requirements.txt
 npm ci --dev
-./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --publ>
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput

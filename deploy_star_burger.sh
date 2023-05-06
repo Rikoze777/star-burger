aa7142ec95b118794594d1fc176552467d97b073
Rikoze777 <spawn00007@gmail.com>
2023-05-06 18:44:38 +0500

    rollbar deploy

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -5,6 +5,7 @@ cd /opt/star-burger
 git pull
 . ./venv/bin/activate
 pip install -r requirements.txt
+npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --publ>
 
 python manage.py collectstatic --noinput
@@ -12,5 +13,11 @@ python manage.py migrate --noinput
 
 systemctl restart burger.service
 systemctl reload nginx.service
+
+last_commit=$(git rev-parse HEAD)
+curl -H "X-Rollbar-Access-Token: d4d0d9071b6b44308b55198dd65ee649" \
+     -H "Content-Type: application/json" \
+     -X POST 'https://api.rollbar.com/api/1/deploy' \
+     -d '{"environment": "production", "revision": "'${last_commit}'", "rollbar_username": "spawn00007", "status": "succeeded"}'
 
 echo Successful

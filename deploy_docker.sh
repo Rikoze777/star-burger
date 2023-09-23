bc4bd898b7b9124beefeaafc6d4280fea4ff533c
Rikoze777 <spawn00007@gmail.com>
2023-09-23 14:29:18 +0500

    chore: add docker for dev and prod

---
+++ deploy_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+mkdir -p staticfiles
+mkdir -p media
+git pull
+
+cd docker_production
+docker-compose build
+docker-compose pull && docker-compose up -d
+
+echo Deploy Docker was successful
